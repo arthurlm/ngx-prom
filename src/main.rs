864c21da4495a0f19f8cebe173a58f043a2105d5
Arthur LE MOIGNE <me@alemoigne.fr>
2021-02-27 12:28:16 +0100

    Switch to clap 3

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod reader;
 
 use actix_web::{get, middleware::Logger, App, HttpResponse, HttpServer};
 use actix_web_prom::PrometheusMetrics;
+use clap::Clap;
 use dotenv::dotenv;
 use std::{io, thread};
 
@@ -16,57 +17,46 @@ fn health() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+/// Nginx to Prometheus basic metrics exporter
+#[derive(Clap)]
+#[clap(version = "0.1")]
+struct Opts {
+    /// Access log file to attach
+    access_log: String,
+
+    /// Prometheus namespace to prefix metrics with
+    #[clap(short, long, default_value = "nginx")]
+    namespace: String,
+
+    /// Bind server to this address and port
+    #[clap(short, long, default_value = "0.0.0.0:5000")]
+    address: String,
+}
+
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    let matches = clap::App::new("Nginx to Prometheus")
-        .version("0.1")
-        .about("Nginx to Prometheus basic metrics exporter")
-        .arg(
-            clap::Arg::with_name("access_log")
-                .help("Access log file to attach")
-                .required(true),
-        )
-        .arg(
-            clap::Arg::with_name("namespace")
-                .short("n")
-                .long("namespace")
-                .help("Prometheus namespace to prefix metrics with")
-                .required(false)
-                .takes_value(true)
-                .default_value("nginx"),
-        )
-        .arg(
-            clap::Arg::with_name("address")
-                .short("a")
-                .long("address")
-                .help("Bind server to this address and port")
-                .required(false)
-                .takes_value(true)
-                .default_value("0.0.0.0:5000"),
-        )
-        .get_matches();
+    let opts = Opts::parse();
 
-    let filename = matches
-        .value_of("access_log")
-        .expect("Cannot read arg")
-        .to_owned();
-    let namespace = matches.value_of("namespace").expect("Cannot read arg");
+    /*
     let server_addr = matches.value_of("address").expect("Cannot read arg");
+    */
 
     let prometheus = PrometheusMetrics::new("api", Some("/metrics"), None);
-    let metrics = metrics::Metrics::new(namespace);
+    let metrics = metrics::Metrics::new(&opts.namespace);
     metrics.register(&prometheus.registry);
 
     init_helpers::fill_counter(&metrics.http_response_total);
+
+    let access_log = opts.access_log.clone();
 
     thread::Builder::new()
         .name("parser".to_owned())
         .spawn(move || {
             let _guard = PanicGuard::new();
-            crate::reader::attach_file(filename, metrics).expect("Parser panic");
+            crate::reader::attach_file(&access_log, metrics).expect("Parser panic");
         })?;
 
     HttpServer::new(move || {
@@ -75,7 +65,7 @@ async fn main() -> io::Result<()> {
             .wrap(prometheus.clone())
             .service(health)
     })
-    .bind(server_addr)?
+    .bind(&opts.address)?
     .run()
     .await
 }
